f4edf24484ecd943fef7c7fe33fb567c2d4ec042
SebastiaN <odm_sd@hotmail.com>
2018-07-10 05:11:13 -0500

    correccion de estilos de invitado y de usuario

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -2,7 +2,7 @@
 @section('titulo') Recuperar cuenta @endsection
 @section('cuerpo')
     <div class="m-grid m-grid--hor m-grid--root m-page">
-        <div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--forget-password m-login--2 m-login-2--skin-2" style="background-image: url(../../../assets/app/media/img//bg/bg-2.jpg);">
+        <div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--forget-password m-login--2 m-login-2--skin-2" >
             <div class="m-grid__item m-grid__item--fluid	m-login__wrapper">
                 <div class="m-login__container">
                     @component('componentes.invitado.logo')
