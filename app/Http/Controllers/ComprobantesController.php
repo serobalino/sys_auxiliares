36ec88d8c166cb37c4bcd4c10e090662a65d59aa
SebastiaN <odm_sd@hotmail.com>
2019-07-20 01:35:53 -0500

    serielizar comprobante en un key info

--- app/Http/Controllers/ComprobantesController.php
+++ app/Http/Controllers/ComprobantesController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Cliente;
 use App\Comprobante;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
-use PhpParser\Node\Expr\Cast\Object_;
+use Jenssegers\Date\Date;
 use SoapClient;
 
 class ComprobantesController extends Controller
@@ -39,33 +39,50 @@ class ComprobantesController extends Controller
      */
     public function store(Request $request)
     {
+        $guardados=0;
+        $existentes=0;
+        $nopertenece=0;
+        $son=0;
+
         $file       =   $request->file('archivo');
         $archivo    =   file_get_contents($file);
         $archivo    =   str_replace(PHP_EOL, ' ', $archivo);
         $elementos  =   explode("	",$archivo);
         $lista      =   [];
         foreach ($elementos as $item){
-            if(strlen($item)===49){
-                $objeto     =  Object_::class( $this->consultar($item)->original);
-                $lista[]    =   ["clave"=>$item,"comprobante"=>$objeto];
-                /*
+            if(strlen($item)===49 && (int)$item>0){
+                $son++;
                 $comprobante    = Comprobante::find($item);
                 if($comprobante){
-
+                    $existentes++;
                 }else{
-                    $sri                        =   $this->consultar($item)->original;
-
-                    $comprobante                =   new Comprobante();
-                    $comprobante->id_co         =   $item;
-                    $comprobante->id_tc         =   $item;
-                    $comprobante->fecha_co      =   $item;
-                    $comprobante->estado_co     =   $item;
-                    $comprobante->comprobante   =   $item;
-
-                }*/
+                    $sri                        =   $this->consultar($item);
+                    $cliente                    =   Cliente::where("dni_cl",$sri->info->identificacionComprador)->orWhere("ruc_cl",$sri->info->identificacionComprador)->first();
+                    if($cliente && @$sri->val){
+                        unset($sri->val);
+                        $comprobante                =   new Comprobante();
+                        $comprobante->id_co         =   $item;
+                        $comprobante->id_tc         =   (int) $sri->infoTributaria->codDoc;
+                        $comprobante->id_cl         =   $cliente->id_cl;
+                        $comprobante->fecha_co      =   Date::createFromFormat('d/m/Y',$sri->info->fechaEmision)->format("Y-m-d");
+                        $comprobante->estado_co     =   true;
+                        $comprobante->comprobante   =   $sri;
+                        $comprobante->save();
+                        $guardados++;
+                    }else{
+                        $errores[]  = ["consulta"=>$sri,"comprobante"=>$item];
+                        $nopertenece++;
+                    }
+                }
             }
         }
-        return response($lista);
+        return response([
+            "guardados"     =>  $guardados,
+            "existentes"    =>  $existentes,
+            "nopertenece"   =>  $nopertenece,
+            "son"           =>  $son,
+            "errores"       =>  @$errores
+        ]);
     }
 
     /**
@@ -146,22 +163,60 @@ class ComprobantesController extends Controller
                 'user_agent'=>'PHPSoapClient'
             )
         );
-
         $context=stream_context_create($opts);
-
         $soapClientOptions=array(
             'stream_context'=>$context,
             'cache_wsdl'=>WSDL_CACHE_NONE
         );
-
         $url="https://cel.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantes?wsdl";
         try {
             $client = new SoapClient($url, $soapClientOptions);
             $aux=$client->autorizacionComprobante(['claveAccesoComprobante'=>$claveAceso]);
-            //$aux->RespuestaAutorizacionComprobante->autorizaciones->autorizacion->comprobante_parseado=simplexml_load_string($aux->RespuestaAutorizacionComprobante->autorizaciones->autorizacion->comprobante);
-            return response()->json(simplexml_load_string(@$aux->RespuestaAutorizacionComprobante->autorizaciones->autorizacion->comprobante));
-        } catch (\SoapFault $e) {
-            return response($e,500);
+            $aux=simplexml_load_string($aux->RespuestaAutorizacionComprobante->autorizaciones->autorizacion->comprobante);
+            $json=json_decode(json_encode($aux));
+            $json->val=true;
+
+            switch ((int)$json->infoTributaria->codDoc) {
+                case 1://factura
+                    $json->info=$json->infoFactura;
+                    unset($json->infoFactura);
+                    break;
+                case 2://nota de venta
+                    $json->info=$json->infoNotaVenta;
+                    unset($json->infoNotaVenta);
+                    break;
+                case 3://liquidacion de compra
+                    $json->info=$json->infoFactura;
+                    unset($json->infoFactura);
+                    break;
+                case 4://nota de credito
+                    $json->info=$json->infoNotaCredito;
+                    unset($json->infoNotaCredito);
+                    break;
+                case 5://nota de debito
+                    $json->info=$json->infoNotaDebito;
+                    unset($json->infoNotaDebito);
+                    break;
+                case 6://guia de remision
+                    $json->info=$json->infoFactura;
+                    unset($json->infoFactura);
+                    break;
+                case 7://comprobante de retencion
+                    $json->info=$json->infoCompRetencion;
+                    unset($json->infoCompRetencion);
+                    break;
+                case 8://entradas a espectaculos
+                    $json->info=$json->infoFactura;
+                    unset($json->infoFactura);
+                    break;
+            }
+
+
+            return $json;
+        } catch (\Exception $e) {
+            $aux["val"]=false;
+            $aux["message"]=$e->getMessage();
+            return $aux;
         }
 
     }
