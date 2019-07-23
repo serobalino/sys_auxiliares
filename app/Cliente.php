d1e99c9a05d9024d8761c12e67612fceee8e63fe
SebastiaN <odm_sd@hotmail.com>
2019-07-23 00:59:44 -0500

    codificacion de cliente

--- app/Cliente.php
+++ app/Cliente.php
@@ -8,4 +8,10 @@ class Cliente extends Model
 {
     protected $primaryKey   =   "id_cl";
 
+    protected $appends = ['codigo'];
+
+    public function getCodigoAttribute(){
+        return md5(sha1($this->attributes['id_cl']));
+    }
+
 }
