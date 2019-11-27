b5ef42b4b8b064ff838eaa5febea47a7b30aff8d
SebastiaN <odm_sd@hotmail.com>
2019-11-27 01:14:14 -0500

    compilacion

--- public/js/app.js
+++ public/js/app.js
@@ -122040,8 +122040,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Documents\Laravel\sys_auxiliares\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Documents\Laravel\sys_auxiliares\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! H:\Documents\GitHub\sys_auxiliares\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! H:\Documents\GitHub\sys_auxiliares\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
