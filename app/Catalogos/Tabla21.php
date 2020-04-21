236182ef28ee3e6cb335af20f76c30f766dc0f13
Sebastian <sebastian.robalino@inpsercom.com>
2020-04-21 11:40:03 -0500

    modelo de tabla 21

---
+++ app/Catalogos/Tabla21.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Catalogos;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tabla21 extends Model
+{
+    protected $table        =   "tabla21";
+    protected $primaryKey   =   "cod_t21";
+    public $timestamps      =   false;
+
+    protected $appends      =   ['label'];
+
+    public function getLabelAttribute()
+    {
+        return "Retención de ".$this->attributes['porcentaje_t21']."%";
+    }
+}
