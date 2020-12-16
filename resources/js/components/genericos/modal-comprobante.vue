f7bcdceae76929708e78bf57137634e306678b8c
Sebastian <odm_sd@hotmail.com>
2020-12-16 15:29:55 -0500

    i==uso de compoenete de json a tabla y cambio porcheckbox

--- resources/js/components/genericos/modal-comprobante.vue
+++ resources/js/components/genericos/modal-comprobante.vue
@@ -1,18 +1,27 @@
 <template>
-    <json-view :data="datos.comprobante" rootKey="comprobante" :maxDepth="1"></json-view>
+    <div>
+        <input type="checkbox" name="tabla" v-model="tabla"> Tabla
+        <vue-json-to-table v-if="tabla" :data="datos.comprobante"></vue-json-to-table>
+        <json-view v-else :data="datos.comprobante" rootKey="comprobante" :maxDepth="1"></json-view>
+    </div>
 </template>
 
 <script>
-    import { JSONView } from "vue-json-component";
-    export default {
-        name: "modal-comprobante",
-        components: { "json-view": JSONView },
-        props:{
-            datos:{
-                type:Object
-            }
+import VueJsonToTable from 'vue-json-to-table';
+import { JSONView } from "vue-json-component";
+Vue.use(VueJsonToTable);
+export default {
+    name: "modal-comprobante",
+    components: { "json-view": JSONView },
+    props: {
+        datos: {
+            type: Object
         }
-    }
+    },
+    data: ()=>({
+        tabla: true
+    })
+}
 </script>
 
 <style scoped>
